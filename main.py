53573e9214c9947c32b552c1af960a015aad4dbc
Djekson <alekssyvorov29@gmail.com>
2023-10-31 18:05:02 +0200

    Version 2.0

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
-#print("1 Задания. Показать на экран все простые числа в диапазоне, указанном пользователем."
-#      "Число называется простым, если оно делится без остатка только на себя и на еди-"
-#      "ницу. Например, три — это простое число, а четыре нет.")
-#i = int(input("Enter numbers: "))
-#for i in range(2, i + 1):
-#    a = True
-#    for b in range(2, i):
-#        if i % b == 0:
-#            a = True
-#            break
-#    if a:
-#       print(i, end='')
-print("2 Задания. Показать на экране таблицу умножения для всех чисел от 1 до 10.")
-for k in range(1, 11):
-    for g in range(1, 11):
-        print(f'{k} * {g} = {k * g}', end='     ')
-    print()
-print("3 Задания. Показать на экран таблицу умножения в диапазоне, указанном пользователем. Например, если пользователь указал 3 и 5")
-for i in range(int(input('Enter numbers: ')),int(input('Enter end: '))+1):
-   print()
-   for j in range(1,11):
-       print(f'{i}*{j}={i*j}',end=' ')
+# Пользователь вводит число. Определить количество цифр в этом числе,
+# посчитать их сумму и среднее арифметическое. Определить количество
+# нулей в этом числе. Общение с пользователем организовать через меню.
+
+# while True:
+#     a = int(input('A or -1')) # 1234
+#     counter_number = 0
+#     counter_zero = 0
+#     summa = 0
+#     while a > 0:
+#         summa += a % 10
+#         counter_number += 1
+#         if a % 10 == 0:
+#             counter_zero += 1
+#         a = a // 10
+#     if a == -1:
+#         print("Exit")
+#         break
+#     print(f'Count number {counter_number}\n'
+#           f'Summa = {summa}\n'
+#           f'Even = {summa / counter_number}\n'
+#           f'Count zero = {counter_zero}')
+while True:
+    a = input('A or -1')
+    summa = 0
+    count_zero = 0
+    count_number = 0
+    for i in a:
+        count_number += 1
+        summa += int(i)
+        if i == "0":
+            count_zero += 1
+    print(f'Count number {count_number}\n'
+          f'Summa = {summa}\n'
+          f'Even = {summa / count_number}\n'
+          f'Count zero = {count_zero}')
+    if a == "0":
+        print("Exit")
+        break
